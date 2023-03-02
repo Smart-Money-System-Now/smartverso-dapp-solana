b00c55560151d00ec06eb791db4f8d7928f305e8
esviren <st-rodriguez@hotmail.com>
2023-03-02 14:43:46 -0500

    Add 3d spline componet

--- web/pages/my-nfts.js
+++ web/pages/my-nfts.js
@@ -2,8 +2,6 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import React,{useEffect, useState} from "react";
-import Spline from '@splinetool/react-spline';
-
 
 export default function Home() {
   const [tikets, setTikets] = useState([]);
